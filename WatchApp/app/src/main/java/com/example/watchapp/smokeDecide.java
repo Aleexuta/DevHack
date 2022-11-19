0e570323212a6fc6e25e48a001f5ca6eac87c8ba
Sergiu Amzuloiu <48670769+Sergiuas@users.noreply.github.com>
2022-11-20 00:14:05 +0200

    ceva

---
+++ WatchApp/app/src/main/java/com/example/watchapp/smokeDecide.java
@@ -0,0 +1,83 @@
+package com.example.watchapp;
+import java.util.Calendar;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class smokeDecide extends Activity {
+
+    int ciggaretesLeft;
+    int hour;
+    int currentHour=Calendar.HOUR_OF_DAY;
+    //You need to put the values!!!
+    TextView question;
+    Button yesBtn;
+    Button noBtn;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.smoke_decide);
+
+        question=(TextView) findViewById(R.id.question);
+        yesBtn=(Button) findViewById(R.id.yesBtn);
+        noBtn=(Button) findViewById(R.id.noBtn);
+        if(currentHour-hour>0)
+        {
+            question.setText("Are you sure you dont want to delay this?");
+            yesBtn.setText("I think I will delay this");
+            noBtn.setText("I really want to smoke");
+
+            noBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    //go back to smoking menu
+
+                }
+            });
+
+            yesBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    question.setText("Do you want suggestions instead?");
+                    yesBtn.setText("Sure");
+                    noBtn.setText("Not really");
+
+                }
+            });
+        }
+        else
+        {
+            question.setText("Don't you believe it's to soon for another one?");
+            yesBtn.setText("Abort");
+            noBtn.setText("I don't care");
+
+            noBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    //go back to smoking menu
+
+                }
+            });
+
+            yesBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    question.setText("Do you want suggestions instead?");
+                    yesBtn.setText("Sure");
+                    noBtn.setText("Not really");
+
+                }
+            });
+        }
+    }
+
+}
--- WatchApp/app/src/main/java/com/example/watchapp/smokingMenu.java
+++ WatchApp/app/src/main/java/com/example/watchapp/smokingMenu.java
@@ -1,16 +1,51 @@
 package com.example.watchapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class smokingMenu extends Activity {
 
+    TextView ciggaretesText;
+    TextView timeText;
+    int ciggaretesLeft;
+    int hour;
+    Button smokeBtn;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.smoking_menu);}
+        setContentView(R.layout.smoking_menu);
+        ciggaretesText=(TextView)findViewById(R.id.ciggaretesText);
+        timeText=(TextView)findViewById(R.id.timeText);
+
+        //Get the value of ciggaretesLeft and last time you smoked;
+
+        ciggaretesText.setText(new String().valueOf((ciggaretesLeft)));
+        timeText.setText(new String().valueOf((hour)));
+
+        smokeBtn= (Button) findViewById(R.id.smokeBtn);
+        smokeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(smokingMenu.this, smokeDecide.class);
+                startActivity(intent);
+            }
+        });
+        //Modify the values after function if the answer was affirmative
+        // ciggaretesLeft=? hour=?
+        ciggaretesText.setText(new String().valueOf((ciggaretesLeft)));
+        timeText.setText(new String().valueOf((hour)));
+
+
+
+    }
 
 }
